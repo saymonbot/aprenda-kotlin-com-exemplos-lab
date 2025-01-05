b8b4a2b4064ae207af2b59937fe13e27d0dc624a
Saymon Botelho <82913820+saymonbot@users.noreply.github.com>
2025-01-05 20:31:32 -0300

    Nivel

--- desafio.kt
+++ desafio.kt
@@ -1,10 +1,13 @@
+enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+
 class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel)
 
 data class Formacao(
     val nome: String,
     val conteudos: List<ConteudoEducacional>,
+    val nivel: List<Nivel>,
 	val inscritos: MutableList<Usuario> = mutableListOf(),
 ) {    
     fun matricular(usuario: Usuario) {
@@ -15,13 +18,16 @@ data class Formacao(
 fun main() {
   val usuario1 = Usuario("Saymon")
   
-  val conteudo1 = ConteudoEducacional("POO", 60)
+  val conteudo1 = ConteudoEducacional("POO", 60, Nivel.INTERMEDIARIO)
   
-  val formacao1 = Formacao("Java", listOf(conteudo1))
+  val formacao1 = Formacao("Java", listOf(conteudo1), listOf(Nivel.INTERMEDIARIO))
   
   formacao1.matricular(usuario1)
   
-  println("Alunos matriculados na formação ${formacao1.nome}: ")
-  formacao1.inscritos.forEach { aluno -> println("Nome: ${aluno.nome}")}
+  println("Formações:")
+  println("${formacao1.nome}, ${formacao1.nivel}")
+  println("")
   
+  println("Alunos matriculados na formação ${formacao1.nome}: ")
+  formacao1.inscritos.forEach { aluno -> println("${aluno.nome}")}
 }
